75308309952e01eba4524ac1fc2814b6cfcf1045
samweihong <weihongsam2002@gmail.com>
2022-02-17 01:15:50 +0800

    :sparkles: Add 1.2.1 Linear Recursion and Iteration

---
+++ src/chapter1/section2/1_Linear_Recursion_and_Iteration.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(factorial 6)
+
+(define (factorial2 n)
+  (define (iter product counter)
+    (if (> counter n)
+      product
+      (iter (* product counter)
+            (+ counter 1))))  
+  (iter 1 1))
+
+(factorial2 6)
+  
