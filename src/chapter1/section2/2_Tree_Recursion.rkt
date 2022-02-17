1573d6cc2090eb19b05937be1b03c10edb95681d
samweihong <weihongsam2002@gmail.com>
2022-02-17 21:58:21 +0800

    :sparkles: Add 1.2.2 Tree Recursion

---
+++ src/chapter1/section2/2_Tree_Recursion.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+; Fibonacci numbers computed using recursive process.
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(fib 5)
+
+; Fibonacci numbers computed using iterative process.
+(define (fib2 n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+(fib2 5)
+
+; Compute number of ways to change an amount of money.
+(define (count-change amount)
+  (define (cc amount kinds-of-coins)
+    (cond ((= amount 0) 1)
+          ((or (< amount 0) (= kinds-of-coins 0)) 0)
+          (else (+ (cc amount
+                       (- kinds-of-coins 1))
+                   (cc (- amount
+                          (first-denomination kinds-of-coins))
+                       kinds-of-coins)))))
+  (define (first-denomination kinds-of-coins)
+    (cond ((= kinds-of-coins 1) 1)
+          ((= kinds-of-coins 2) 5)
+          ((= kinds-of-coins 3) 10)
+          ((= kinds-of-coins 4) 20)
+          ((= kinds-of-coins 5) 50)))
+  (cc amount 5))
+ 
+(count-change 100)
