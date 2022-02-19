0cabfa68bab9ac9f7910687c35495c09d6059064
samweihong <weihongsam2002@gmail.com>
2022-02-20 01:45:56 +0800

    :beers: Add Exercise 1.14 - 1.15

---
+++ src/chapter1/Exercise_1.15.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(sine 1)
+(sine 3.14159)
+(sine 100)
+
+(sine 12.15)
