0a3fdc1aba5d1d63789ab258529c1b99ba9b84af
samweihong <weihongsam2002@gmail.com>
2022-02-20 21:45:30 +0800

    :sparkles: Add 1.2.4 Exponentiation

---
+++ src/chapter1/section2/4_Exponentiation.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+; Recursive process
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+; Iterative process
+(define (expt2 b n)
+  (define (expt-iter counter product)
+    (if (= counter 0)
+        product
+        (expt-iter (- counter 1)
+                   (* b product))))
+  (expt-iter n 1))
+
+(define (fast-expt b n)
+  (define (even? n)
+    (= (remainder n 2) 0))
+  (define (square n)
+    (* n n))
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(expt 3 5)
+(expt2 3 5)
+(fast-expt 3 5)
+
+(expt 2 1000)
+(expt2 2 1000)
+(fast-expt 2 1000)
