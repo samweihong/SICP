b01f857880bf73344a4d0f0453d34b1c753d3d54
Sam Wei Hong <weihongsam2002@gmail.com>
2022-08-24 20:34:59 +0800

    :sparkles: Add 1.3.3 Procedures as General Methods

---
+++ src/chapter1/section3/3_Procedures_as_General_Methods.rkt
@@ -0,0 +1,56 @@
+#lang sicp
+
+; Finding roots of equations by the half-interval method
+
+(define (average a b) (/ (+ a b) 2))
+
+(define (search f neg-point pos-point)  
+  (define (close-enough? x y) (< (abs (- x y)) 0.001))
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+            (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+; Finding fixed points of functions
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+(sqrt 2)
