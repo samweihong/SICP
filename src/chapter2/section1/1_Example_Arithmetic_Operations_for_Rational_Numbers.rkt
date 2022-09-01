d3c719dcadd803625306b6d51b7caa1fe29bf924
Sam Wei Hong <weihongsam2002@gmail.com>
2022-09-01 14:57:06 +0800

    :sparkles: Add 2.1.1 Example: Arithmetic Operations for Rational Numbers

---
+++ src/chapter2/section1/1_Example_Arithmetic_Operations_for_Rational_Numbers.rkt
@@ -0,0 +1,58 @@
+#lang sicp
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; Pairs
+
+(define x (cons 1 2))
+(car x)
+(cdr x)
+
+(define y (cons 3 4))
+(define z (cons x y))
+(car (car z))
+(car (cdr z))
+
+; Representing rational numbers
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+(print-rat one-third)
+
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
